48073cb13437ee8829047056179d910b7e4b73eb
Mike <mickael.merlet67@gmail.com>
2020-06-22 13:39:38 +0200

    Refactor class and add main menu

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[]args){
-        System.out.println("1. Generate new private key and new public key \n2. Encrypt file\n3. Decrypt file \nChoice: ");
-        int choice = new Scanner(System.in).nextInt();
-        if(choice == 1){
-            RsaKeysGenerator rsaKeysGenerator = new RsaKeysGenerator();
-            RsaKeysGenerator.generateKeys();
-        }
-        if (choice == 2 ){
-            Encryption encryption = new Encryption();
-            Encryption.encryption();
-        }
-        if(choice == 3 ){
-            Decryption decryption = new Decryption();
-            decryption.decryption();
+    public static void main(String[] args) {
+        int choice = 0;
+        while (choice != 4) {
+            System.out.println("1. Generate new private key and new public key \n2. Encrypt file\n3. Decrypt file\n4. Exit \nChoice: ");
+            choice = new Scanner(System.in).nextInt();
+            if (choice == 1) {
+                RsaKeysGenerator rsaKeysGenerator = new RsaKeysGenerator();
+                RsaKeysGenerator.generateKeys();
+            }
+            if (choice == 2) {
+                Encryption encryption = new Encryption();
+                Encryption.encryption();
+            }
+            if (choice == 3) {
+                Decryption decryption = new Decryption();
+                decryption.decryption();
+            }
+            if (choice == 4){
+                System.exit(-1);
+            }
         }
     }
 }
