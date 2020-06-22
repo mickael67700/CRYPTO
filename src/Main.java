87dc0f5ac550c4da9c9b9f133c5ce2bfc744f37e
Mike <mickael.merlet67@gmail.com>
2020-06-22 13:47:24 +0200

    Static access in main method

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,13 @@ public class Main {
             System.out.println("1. Generate new private key and new public key \n2. Encrypt file\n3. Decrypt file\n4. Exit \nChoice: ");
             choice = new Scanner(System.in).nextInt();
             if (choice == 1) {
-                RsaKeysGenerator rsaKeysGenerator = new RsaKeysGenerator();
                 RsaKeysGenerator.generateKeys();
             }
             if (choice == 2) {
-                Encryption encryption = new Encryption();
                 Encryption.encryption();
             }
             if (choice == 3) {
-                Decryption decryption = new Decryption();
-                decryption.decryption();
+                Decryption.decryption();
             }
             if (choice == 4){
                 System.exit(-1);
